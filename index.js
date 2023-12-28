38e0208147014809aaa238ffe1dcf4fcd36e2442
YAZEEDKH\yazee <yazeedkhanfer02@gmail.com>
2023-12-28 19:39:06 +0200

    Update the index file and server

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ app.use(bodyParser.json());
 const authRoutes = require('./Auth/auth');
 const locationsRouter = require('./Data/locationsRouter');
 
-
+const educationalResourcesRouter = require('./EducationalResources/educationalResourcesRouter')
+const communityRouter = require('./Community Reporting/communityRouter');
 app.use('/auth', authRoutes);
 app.use('/dataCOll', locationsRouter);
-
+app.use('/edResource', educationalResourcesRouter);
+app.use('/community', communityRouter);
 
 app.listen(3080, () => {
     console.log(`Server running in port ${3080}/`);
